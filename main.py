82a4bbf8fa6b4e286f814e1dce91d1c182ce7d97
MeiirimZh <zh.meyirim@gmail.com>
2025-03-26 18:39:27 +0500

    integrated pygame and configured screen properties

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+from sys import exit
+import pygame
+
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, FPS
+
+
 class Game:
     def __init__(self):
         self.game_state_manager = GameStateManager("Player vs Computer")
 
+        pygame.init()
+        self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
+        pygame.display.set_caption(GAME_TITLE)
+        self.clock = pygame.time.Clock()
+
     def run(self):
         while True:
-            pass
+            pygame.mouse.set_visible(False)
+
+            self.display.fill((0, 0, 0))
+
+            events = pygame.event.get()
+
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 
 class GameStateManager:
