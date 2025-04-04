26e492ebebc74a6ffb5b2e78ce9788f1fd47b4e4
MeiirimZh <zh.meyirim@gmail.com>
2025-04-04 14:24:00 +0500

    can change background color in settings now

--- data.py
+++ data.py
@@ -8,12 +8,12 @@ class Data:
 
         self.on_fullscreen = False
 
-        self.bg_color = (0, 0, 0)
-        self.player_1_color = (255, 255, 255)
-        self.player_2_color = (255, 255, 255)
-        self.ball_color = (255, 255, 255)
-        self.scores_color = (50, 50, 50)
-        self.divider_color = (50, 50, 50)
+        self.bg_color = [0, 0, 0]
+        self.player_1_color = [255, 255, 255]
+        self.player_2_color = [255, 255, 255]
+        self.ball_color = [255, 255, 255]
+        self.scores_color = [50, 50, 50]
+        self.divider_color = [50, 50, 50]
 
         self.score_font = pygame.font.Font("fonts/Silkscreen-Regular.ttf", 50)
         self.title_font = pygame.font.Font("fonts/Silkscreen-Regular.ttf", 128)
--- scenes/settings.py
+++ scenes/settings.py
@@ -9,25 +9,27 @@ class Settings:
         self.data = data
         self.display = display
 
+        self.distance_color_channels = 80
+
         self.title = self.data.medium_font.render("Settings", True, (255, 255, 255))
 
         self.options = ["Fullscreen", "Background color", "Return to menu"]
-        self.option_pos = [(400, 400), (400, 460), (400, 520)]
+        self.option_pos = [(400, 400), (400, 440), (400, 480)]
         self.current_option = 0
 
-    def run(self, events):
-        self.display.fill((0, 0, 0))
+        self.color_channel = 0
 
-        self.display.blit(self.title, (477, 40))
+        self.bg_color_red = self.data.setting_font.render(str(self.data.bg_color[0]), True, (255, 255, 255))
+        self.bg_color_green = self.data.setting_font.render(str(self.data.bg_color[1]), True, (255, 255, 255))
+        self.bg_color_blue = self.data.setting_font.render(str(self.data.bg_color[2]), True, (255, 255, 255))
+        self.bg_color_channels = [self.bg_color_red, self.bg_color_green, self.bg_color_blue]
+
+        self.change_bg = False
+
+    def run(self, events):
+        self.render()
 
         self.options[0] = 'Fullscreen: ON' if self.data.on_fullscreen else 'Fullscreen: OFF'
-
-        for option in self.options:
-            if option == self.options[self.current_option]:
-                text = self.data.setting_font.render(option, True, (255, 0, 0))
-            else:
-                text = self.data.setting_font.render(option, True, (255, 255, 255))
-            self.display.blit(text, self.option_pos[self.options.index(option)])
 
         for event in events:
             if event.type == pygame.KEYDOWN:
@@ -37,6 +39,13 @@ class Settings:
                 if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     self.current_option = min(len(self.options) - 1, self.current_option + 1)
                     self.data.select_sound.play()
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    self.color_channel = max(0, self.color_channel - 1)
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    self.color_channel = min(2, self.color_channel + 1)
+                if event.unicode.isdigit():
+                    if self.current_option in (1,):
+                        self.enter_number(self.current_option, self.color_channel, event.unicode)
                 if event.key == pygame.K_RETURN:
                     if self.current_option == 0:
                         self.data.on_fullscreen = not self.data.on_fullscreen
@@ -46,3 +55,43 @@ class Settings:
                             self.display = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
                     if self.current_option == 2:
                         self.game_state_manager.set_state("Main Menu")
+
+    def render(self):
+        self.display.fill((0, 0, 0))
+
+        self.display.blit(self.title, (477, 40))
+
+        for option in self.options:
+            if option == self.options[self.current_option]:
+                text = self.data.setting_font.render(option, True, (255, 0, 0))
+            else:
+                text = self.data.setting_font.render(option, True, (255, 255, 255))
+            self.display.blit(text, self.option_pos[self.options.index(option)])
+
+        for i in range(3):
+            if i == self.color_channel and self.current_option == 1:
+                text = self.data.setting_font.render(str(self.data.bg_color[i]), True, (255, 0, 0))
+            else:
+                text = self.data.setting_font.render(str(self.data.bg_color[i]), True, (255, 255, 255))
+            self.display.blit(text, (750 + i * self.distance_color_channels, 440))
+
+    def enter_number(self, option, color_channel, event_unicode):
+        # Background color
+        if option == 1:
+            current_number = self.data.bg_color[color_channel]
+        
+        if self.can_change_channel(current_number):
+            if current_number == 0:
+                current_number = event_unicode
+            else:
+                current_number = str(current_number) + event_unicode
+
+                if int(current_number) > 255:
+                    current_number = 255
+
+        # Background color
+        if option == 1:
+            self.data.bg_color[color_channel] = int(current_number)
+
+    def can_change_channel(self, channel):
+        return len(str(channel)) != 3
